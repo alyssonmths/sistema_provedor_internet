ecc189f61904f4b875bb3f4d1f70554a15630d11
Alysson <alyssonmths5@gmail.com>
2025-10-14 10:07:28 -0300

    criação de métodos cadastrar e atualizar

--- src/main/java/org/example/Plano.java
+++ src/main/java/org/example/Plano.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Plano {
 
     private String nome;
@@ -7,11 +9,33 @@ public class Plano {
     private double velocidade;
     private double preco;
 
+    public Plano() {
+
+    }
+
     public Plano(String nome, String descricao, double velocidade, double preco) {
         this.nome = nome;
         this.descricao = descricao;
         this.velocidade = velocidade;
         this.preco = preco;
+    }
+
+    public void cadastrar() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nome: ");
+        this.nome = sc.nextLine();
+
+        System.out.println("Descrição: ");
+        this.descricao = sc.nextLine();
+
+        System.out.println("Contato: ");
+        this.velocidade = sc.nextDouble();
+
+        System.out.println("Preço: ");
+        this.preco = sc.nextDouble();
+
     }
 
     public void atualizarPreco(double novoPreco) {
@@ -22,9 +46,49 @@ public class Plano {
         this.velocidade = velocidade;
     }
 
+    public static void atualizarDados() {
+
+        System.out.println("Selecione o plano: ");
+        System.out.println("Nome: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome = sc.nextLine();
+
+        // LÓGICA PARA PESQUISAR O PLANO
+
+        Plano plano = new Plano("Teste", "Teste", 1, 1);
+
+        System.out.println("Dados do plano selecionado");
+
+        System.out.println(plano);
+
+        System.out.println("Nome: ");
+        plano.nome = sc.nextLine();
+
+        System.out.println("Endereço: ");
+        plano.descricao = sc.nextLine();
+
+        System.out.println("Contato: ");
+        plano.velocidade = sc.nextDouble();
+
+        System.out.println("Preço: ");
+        plano.preco = sc.nextDouble();
+
+        // LÓGICA DO BANCO DE DADOS
+    }
+
     public String detalhesPlano() {
         return String.format("Plano: %s\nVelocidade: %.2f Mbps\nPreço: R$ %.2f",
                 nome, velocidade, preco);
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%nNome: %s %nDescrição: %s %nVelocidade: %.2f %nPreço: %.2f%n", nome, descricao, velocidade, preco);
+    }
 }
